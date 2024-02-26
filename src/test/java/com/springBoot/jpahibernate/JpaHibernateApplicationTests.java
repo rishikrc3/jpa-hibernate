d3bb7a6b6a928af5434bb92f471e6e2b1262afff
Rishik Chakravarty <rishikrc3@gmail.com>
2024-02-27 02:38:49 +0530

    connecting_to_h2_database

---
+++ src/test/java/com/springBoot/jpahibernate/JpaHibernateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springBoot.jpahibernate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaHibernateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
